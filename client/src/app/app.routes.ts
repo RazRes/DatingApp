d52715abbd77c2a860b462967a137e10c573e373
razvan.resmerita <resmeritarazvan@gmail.com>
2025-12-08 17:17:10 +0200

    End of routing section

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,23 @@
+import { authGuard } from './../core/guards/auth-guard';
+import { MemmberList } from './../features/members/memmber-list/memmber-list';
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MemmberList},
+            { path: 'members/:id', component: MemberDetailed },
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },
+        ]
+    },
+    { path: '**', component: Home },
+];
---
+++ client/src/features/members/memmber-list/memmber-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memmber-list',
+  imports: [],
+  templateUrl: './memmber-list.html',
+  styleUrl: './memmber-list.css',
+})
+export class MemmberList {
+
+}
